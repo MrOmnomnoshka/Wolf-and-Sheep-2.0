d97a8170dc752f06ecf62438177ea225ce07b866
MrOmnomnoshka <mromnomnoshka@gmail.com>
2020-01-12 22:16:10 +0300

    First commit

---
+++ Assets/Scripts/MapCreator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapCreator : MonoBehaviour
+{
+    public int pointsAmount = 50;
+    public GameObject pointPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+        CheckConnections();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void GenerateMap()
+    {
+        for (int i = 0; i < pointsAmount; i++)
+        {
+            var pointObj = Instantiate(pointPrefab);
+            pointObj.transform.position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-4.0f, 4.0f), 0);
+        }
+    }
+
+    void CheckConnections()
+    {
+        var lines = GameObject.FindGameObjectsWithTag("LineTag");
+
+        foreach (GameObject line in lines)
+        {
+            if (line.GetComponent<LineRenderer>().positionCount == 0)  // if all have pairs
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PointsConnector.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsConnector : MonoBehaviour
+{
+    public GameObject lineRendererObj;
+
+    private LineRenderer lineRenderer;
+    private GameObject[] linesObjects;
+    public GameObject[] connectedPoints;
+    private int[] connectedIndexes; 
+
+    private int maxDistance = 3;
+    //private int minDistance = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var pointsAmount = GameObject.FindGameObjectsWithTag("PointTag").Length;
+        linesObjects = new GameObject[pointsAmount];
+        for (int i = 0; i < pointsAmount; i++)
+        {
+            var tempObject = Instantiate(lineRendererObj);
+            linesObjects[i] = tempObject;
+        }
+    }
+
+
+    void Update()
+    {
+        var points = GameObject.FindGameObjectsWithTag("PointTag");
+
+        connectedIndexes = new int[points.Length];  // array for neighborhoods
+        int indexesCounter = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            indexesCounter = 0;
+            float distance;  // distance to the other point
+
+            lineRenderer = linesObjects[i].GetComponent<LineRenderer>();
+            lineRenderer.positionCount = 0;
+            for (int j = 0; j < points.Length; j++)
+            {
+                if (i != j)
+                {
+                    distance = Vector2.Distance(points[i].transform.position, points[j].transform.position);
+                    if (distance < maxDistance)
+                    {
+                        // attach to other point
+                        lineRenderer.positionCount++;
+                        lineRenderer.SetPosition(lineRenderer.positionCount - 1, points[j].transform.position);
+                        connectedIndexes[indexesCounter] = j;
+                        indexesCounter++;
+
+                        // and go back to self
+                        lineRenderer.positionCount++;
+                        lineRenderer.SetPosition(lineRenderer.positionCount - 1, points[i].transform.position);
+                    }
+                }
+            }
+            
+            PointParms pp = points[i].GetComponent<PointParms>();
+            
+            pp.connectedPoints = new GameObject[indexesCounter];
+            for (int k = 0; k < indexesCounter; k++)
+            {
+                pp.connectedPoints[k] = points[connectedIndexes[k]]; // TODO: Fix it
+            }
+        }
+    }
+}
